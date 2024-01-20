c4cca1eb2da3c8ffdb9fc1904e0613218824c628
achstar <87681108+achstar@users.noreply.github.com>
2024-01-20 14:49:28 -0500

    Update ribbon_code.ino with song code

--- ribbon_code.ino
+++ ribbon_code.ino
@@ -11,12 +11,13 @@
 CRGB leds[NUM_LEDS];
 // minimum and maximum freq range
 const int MIN_FREQ = 130;
-const int MAX_FREQ = 500;
+const int MAX_FREQ = 550;
 
 AutoMap mapRibbonFreq(1, 1023, MAX_FREQ, MIN_FREQ);
-AutoMap mapLEDs(1, 1023, 1, NUM_LEDS);
+AutoMap mapLEDs(1, 1023, NUM_LEDS, 0);
 
-int isOn[NUM_LEDS];
+int song[] = {1, 1, 5, 5, 7, 7, 5, 4, 4, 3, 3, 2, 2, 1, 5, 5, 4, 4, 3, 3, 2, 5, 5, 4, 4, 3, 3, 2, 1, 1, 5, 5, 7, 7, 5, 4, 4, 3, 3, 2, 2, 1};
+int note = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,23 +31,31 @@ void loop() {
   // Serial.println(analogRead(FORCE_PIN));
   // delay(100);
   // noTone(SPEAKER_PIN);
-
   int location = analogRead(RIBBON_PIN);
-  Serial.println(location);
+  Serial.println("freq: " + String(mapRibbonFreq(location)) + " LEDs: " + String(mapLEDs(location)));
   if (location == 0) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      if (isOn[i]) {
-        leds[i] = CRGB::Black;
-        FastLED.show();
-      }
-    }
+    leds[song[note]] = CRGB(255, 255, 255);
+
+    FastLED.show();
     noTone(SPEAKER_PIN);
+  } else if (song[note] == mapLEDs(location)) {
+    leds[song[note]] = CRGB(0, 255, 0);
+
+    FastLED.show();
+    delay(100);
+    leds[song[note]] = CRGB::Black;
+    FastLED.show();
+    if (note < 41) {
+      note++;
+    }
+    else {
+      note = 0;
+    }
+    tone(SPEAKER_PIN, mapRibbonFreq(location));
   }
   else {
-    int led_num = mapLEDs(location);
-    leds[led_num] = CRGB(0, 255, 0);
-    FastLED.show();
-    isOn[led_num] = 1;
+    // leds[song[note]] = CRGB(255, 0, 0);
+    // FastLED.show();
     tone(SPEAKER_PIN, mapRibbonFreq(location));
   }
   
